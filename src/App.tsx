7da83fb76436418706bd4aba40334cd423ad773f
JordanDyck <jordandyck777@hotmail.com>
2025-09-19 12:16:34 -0600

    moved Descriptions into ProjectInfo. added more styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import "./styles/main.scss"
 import ProjectInfo from "./Components/ProjectInfo"
 import me from "./assets/me.png"
-import dnd from "./assets/dnd.png"
-import pokebuddy from "./assets/Pokebuddy.png"
-import css_generator from "./assets/css_generator.png"
-import data from "./Components/Description"
 
 function App() {
-  const images = [dnd, pokebuddy, css_generator]
-
   return (
     <div className="App">
       <header>JORDAN</header>
       <div className="welcome">
-        <h1 className="intro-text">
-          I'm Jordan, a Calgary based junior front-end web developer with over 3 years of experience
-          in Reactjs & TypeScript.
-          <br />I enjoy working with a team and pushing the limits of what can be done. I focus my
-          sites on accessibility and ease of use. there's no need to make a site too complicated.
-        </h1>
         <img className="self-image" src={me} alt="jordan image" />
+        <div className="intro-text">
+          <h1 className="name">{`<I'm JORDAN>`}</h1>
+          <h1 className="carreer">
+            {`<Junior Front-End`} <span>Reactjs/TypeScript</span> {`developer>`}
+          </h1>
+          <h1 className="experience">{`<Over 3 years of experience>`}</h1>
+          <h1 className="about">
+            I started programming because I like being able to create something completely from
+            scratch and seeing it all come together. I enjoy working with a team, and learning new &
+            better ways to solve problems. I focus my sites on <span>accessibility</span> and{" "}
+            <span>ease of use</span>.
+          </h1>
+        </div>
         <div className="links">
           <a href="https://github.com/JordanDyck" target="_blank" rel="noopener noreferrer">
             <button className="github-btn">GitHub</button>
@@ -28,16 +29,8 @@ function App() {
         </div>
       </div>
       <div className="container">
-        <header>Some of my projects:</header>
-        {Object.entries(data).map((project, i) => (
-          <ProjectInfo
-            projectImage={images[i]}
-            name={project[0]}
-            info={project[1].info}
-            key={project[0]}
-            link={project[1].url}
-          />
-        ))}
+        <header className="projects-header">Some of my projects:</header>
+        <ProjectInfo />
       </div>
     </div>
   )
--- src/Components/ProjectInfo.tsx
+++ src/Components/ProjectInfo.tsx
@@ -1,24 +1,63 @@
-interface Tinfo {
-  name: string
-  projectImage: React.ImgHTMLAttributes<HTMLImageElement>["src"]
-  info: string[]
-  link: string
-}
-
-const ProjectInfo = ({name, projectImage, info, link}: Tinfo) => {
+import dnd from "../assets/dnd.png"
+import pokebuddy from "../assets/Pokebuddy.png"
+import css_generator from "../assets/css_generator.png"
+const ProjectInfo = () => {
   return (
-    <div className="project-info-container">
-      <div className="info">
-        <h1>{name.replace("_", " ")}</h1>
-        {info.map((par, i) => (
-          <p key={`${name}_par_${i}`}>{par}</p>
-        ))}
+    <>
+      <div className="project-info-container my-dnd">
+        <div className="info">
+          <h1>My DnD</h1>
+          <p>My biggest project.</p>
+          <p>
+            An interactive Dungeons & Dragons character sheet that allows users to create and manage
+            their characters, track <span>stats</span>, <span>abilities</span>, <span>levels</span>,{" "}
+            <span>inventory</span>, and export them to share on other devices. you can use premade
+            classes/races, or create your own custom character. I spent over 2 years trying to make
+            the easiest to use character manager I could. no confusing menus and easy to read
+            descriptions.
+          </p>
+        </div>
+        <a href={"https://jordandyck.github.io/My-DnD/"} target="_blank" rel="noopener noreferrer">
+          <img className="project-image" src={dnd} alt="project image" />
+        </a>
       </div>
-
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <img className="project-image" src={projectImage} alt="project image" />
-      </a>
-    </div>
+      <div className="project-info-container pokebuddy">
+        <div className="info">
+          <h1>pokebuddy</h1>
+          <p>Search and manage your team of pokemon.</p>
+          <p>
+            This site makes it easy for new players to get into pokemon as the game does not have a
+            way to know the advantages and disadvantages of each pokemon, and looking up the type
+            chart for each pokemon can be confusing and time consuming. Now everything is in one
+            place, making it easy to find the information you need.
+          </p>
+        </div>
+        <a
+          href={"https://jordandyck.github.io/Pokebuddy/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="project-image" src={pokebuddy} alt="project image" />
+        </a>
+      </div>
+      <div className="project-info-container css-generator">
+        <div className="info">
+          <h1>CSS Generator</h1>
+          <p>
+            A css generator that allows new programers to see the differents between padding,
+            margin, and border. you can also use it to generate a box-shadow and copy the code to
+            your own project.
+          </p>
+        </div>
+        <a
+          href={"https://jordandyck.github.io/CSS-Generator/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="project-image" src={css_generator} alt="project image" />
+        </a>
+      </div>
+    </>
   )
 }
 export default ProjectInfo
